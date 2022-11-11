1cd8255fefe6bacc8b06a87bab1b47e16e42241a
lulamae12 <tls.tommy11@gmail.com>
2022-11-11 17:37:57 +0000

    update script

--- scripts/xurTrackerAutoDriver.sh
+++ scripts/xurTrackerAutoDriver.sh
@@ -4,7 +4,7 @@ sudo /usr/bin/python3 /home/ubuntu/XurTracker/src/tweetBotTweet.py
 sudo /usr/bin/python3 /home/ubuntu/XurTracker/src/generateJSON.py
 bash /home/ubuntu/XurTracker/scripts/regenerateHTML.sh
 sudo /usr/bin/python3 /home/ubuntu/XurTracker/src/deleteOldManifests.py
-echo committing!
 git add /home/ubuntu/XurTracker/
 git commit -m "weekly update"
 git push
+echo committed!
