067aef7e402f65c37cf3e61d0e2bbe106905c96a
lulamae12 <tls.tommy11@gmail.com>
2023-02-24 17:46:26 +0000

    added debug logging

--- scripts/startWebserver.sh
+++ scripts/startWebserver.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 sudo pkill gunicorn  
-sudo gunicorn --certfile=/home/ubuntu/XurTracker/data/cert.pem --keyfile=/home/ubuntu/XurTracker/data/key.pem --workers=5 --threads=2 --max-requests 1000 --bind 0.0.0.0:443  --chdir /home/ubuntu/XurTracker wsgi:app
+sudo gunicorn --certfile=/home/ubuntu/XurTracker/data/cert.pem --keyfile=/home/ubuntu/XurTracker/data/key.pem --workers=5 --threads=2 --max-requests 1000 --bind 0.0.0.0:443  --chdir /home/ubuntu/XurTracker --access-logfile /home/ubuntu/XurTracker/data/logger.txt wsgi:app
 #sudo gunicorn --certfile=/home/ubuntu/XurTracker/data/cert.pem --keyfile=/home/ubuntu/XurTracker/data/key.pem --workers=5 --threads=2 --bind 0.0.0.0:443 wsgi:app
