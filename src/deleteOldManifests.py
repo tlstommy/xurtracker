2c7082285207c6224b7b061ea9afb7848bd1952f
lulamae12 <tls.tommy11@gmail.com>
2023-03-24 17:06:32 +0000

    commit manifest changes

--- src/deleteOldManifests.py
+++ src/deleteOldManifests.py
@@ -1,18 +1,18 @@
 import os
 def deleteOldManifests():
     manifestFiles = []
-    files = os.listdir("/home/ubuntu/XurTracker")
+    files = os.listdir("/home/ubuntu/XurTracker/src")
     print(files)
     for f in range(len(files)):
         if files[f].find(".content") != -1:
             manifestFiles.append(files[f])
     newestFile = manifestFiles[0]
     print(newestFile)
-    newestFileTime = os.path.getctime(os.path.join("/home/ubuntu/XurTracker",manifestFiles[0]))
+    newestFileTime = os.path.getctime(os.path.join("/home/ubuntu/XurTracker/src",manifestFiles[0]))
 
     #find the newest .content file in the dir
     for f in range(len(manifestFiles)): 
-        fileCreatedTime = os.path.getctime(os.path.join("/home/ubuntu/XurTracker",manifestFiles[f]))
+        fileCreatedTime = os.path.getctime(os.path.join("/home/ubuntu/XurTracker/src",manifestFiles[f]))
         if(fileCreatedTime > newestFileTime):
             newestFile = manifestFiles[f]
     
@@ -21,6 +21,6 @@ def deleteOldManifests():
 
     #delete all old manifest files from the directory
     for f in range(len(manifestFiles)): 
-        os.remove(os.path.join("/home/ubuntu/XurTracker",manifestFiles[f]))
+        os.remove(os.path.join("/home/ubuntu/XurTracker/src",manifestFiles[f]))
 
 deleteOldManifests()
