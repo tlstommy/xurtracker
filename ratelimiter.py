46f077776793406aff9027a81ab6a07c03252166
lulamae12 <tls.tommy11@gmail.com>
2023-09-22 19:18:55 +0000

    setup redis ratelimits

--- ratelimiter.py
+++ ratelimiter.py
@@ -1,7 +1,10 @@
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
-limiter = Limiter(key_func=get_remote_address)
+limiter = Limiter(
+    key_func=get_remote_address,
+    storage_uri="redis://localhost:6379"
+)
 
 #limit
 limit = "1 per day"
