70e1cdfc60d9233a28ee51fef78d6591a1cb932e
lulamae12 <tls.tommy11@gmail.com>
2023-09-22 19:26:34 +0000

    update gitignore and redis limits

--- ratelimiter.py
+++ ratelimiter.py
@@ -3,9 +3,10 @@ from flask_limiter.util import get_remote_address
 
 limiter = Limiter(
     key_func=get_remote_address,
-    storage_uri="redis://localhost:6379"
+    storage_uri="redis://localhost:6379",
+    strategy="fixed-window"
 )
 
 #limit
-limit = "1 per day"
+limit = "10 per day"
 
