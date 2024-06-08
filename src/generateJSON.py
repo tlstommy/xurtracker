97f51a9b3482141e8f39cc445ed43ecea0d793ed
tlstommy <tls.tommy11@gmail.com>
2024-06-08 02:02:01 +0000

    getting legend and exotic armor now

--- src/generateJSON.py
+++ src/generateJSON.py
@@ -5,6 +5,8 @@ from credentials import *
 
 HEADERS = { "X-API-Key": API_KEY, "Authorization": f"Bearer {ACCESS_TOKEN}" }
 
+#strange gear offers hash 3751514131
+
 class main:
     def __init__(self, apiKey):
         #destiny pydest object
@@ -17,6 +19,7 @@ class main:
         self.hunterCharacterID = characterIDHunter
         self.destinyURLBase = "https://www.bungie.net/Platform"
         self.vendorHash = "2190858386" #xur
+        self.strangeGearVendorHash = "3751514131" #xur's strange gear tab inside of the xurs menu
 
         self.combinedPerksJson = None
 
@@ -588,7 +591,7 @@ class main:
         if hash == 4244567218:
             return "Strength"
 
-    async def getArmor(self,classID,classType):
+    async def getExoticArmor(self,classID,classType):
         #armor stat template
         IDtoArmorHashDict = {}
         socketStatDict = {}
@@ -767,6 +770,160 @@ class main:
             if(classType == "warlock"):
                 if(isExoticBool):
                     self.WarlockExotic = jsonTemplate
+                
+            if(classType == "hunter"):
+                if(isExoticBool):
+                    self.HunterExotic = jsonTemplate
+                
+            if(classType == "titan"):
+                if(isExoticBool):
+                    self.TitanExotic = jsonTemplate
+
+    #get legendary armor from the strange gear offers vendor
+    async def getLegendaryArmor(self,classID,classType):
+        #armor stat template
+        IDtoArmorHashDict = {}
+        socketStatDict = {}
+        itemIDs402 = []
+        socketIDs305 = []
+        currentAvailableItems = []
+        currentAvailableArmorItems = []
+        
+        #item sockets
+        apiUrl304 = self.destinyURLBase + f"/Destiny2/{self.membershipType}/Profile/{self.membershipId}/Character/{classID}/Vendors/{self.strangeGearVendorHash}/?components=304"
+        apiResponse304 = self.get_api_request(apiUrl304)
+        apiResponse304Json = json.loads(apiResponse304)
+
+        #item ids
+        apiUrl402 = self.destinyURLBase + f"/Destiny2/{self.membershipType}/Profile/{self.membershipId}/Character/{classID}/Vendors/{self.strangeGearVendorHash}/?components=402"
+        apiResponse402 = self.get_api_request(apiUrl402)
+        apiResponse402Json = json.loads(apiResponse402)
+        
+        for itemID, itemIDData in apiResponse402Json["Response"]["sales"]["data"].items():
+            itemIDs402.append(itemID)
+            
+        
+        for socketID, socketIDData in apiResponse304Json["Response"]["itemComponents"]["stats"]["data"].items():
+            socketIDs305.append(socketID)
+            socketStatDict[socketID]=socketIDData
+            
+        for i in range(len(itemIDs402)):
+            if itemIDs402[i] in socketIDs305:
+                currentAvailableItems.append(itemIDs402[i])
+        for key, value in apiResponse402Json["Response"]["sales"]["data"].items():
+            if key in currentAvailableItems:
+                IDtoArmorHashDict[key]=value.get("itemHash")
+                
+        #look through currentAvailableItems for armor peices hash and then see if the hash relates to armor or not
+        for i in range(len(currentAvailableItems)):
+            hashData = await self.decodeHash(IDtoArmorHashDict[currentAvailableItems[i]],"DestinyInventoryItemDefinition")
+            #it is armor   
+            if hashData.get("itemType") == 2:
+                currentAvailableArmorItems.append(currentAvailableItems[i])
+
+        #now get rid of the exotics for the other class
+        removalIDS = []
+        for i in range(len(currentAvailableArmorItems)):
+            hashData = await self.decodeHash(IDtoArmorHashDict[currentAvailableArmorItems[i]],"DestinyInventoryItemDefinition")
+
+            #titan
+            if hashData.get("classType") == 0:
+                if(classType != "titan"):
+                    removalIDS.append(currentAvailableArmorItems[i])
+            #hunter
+            if hashData.get("classType") == 1:
+                if(classType != "hunter"):
+                    removalIDS.append(currentAvailableArmorItems[i])
+            #warlock
+            if hashData.get("classType") == 2:
+                if(classType != "warlock"):
+                    removalIDS.append(currentAvailableArmorItems[i])
+
+
+
+        for i in range(len(removalIDS)):
+            currentAvailableArmorItems.remove(removalIDS[i])
+
+        
+
+        #now get each individual armors perks
+        for item in currentAvailableArmorItems:
+            
+
+            #total, mobility, resilince , recovery ,discipline, intelect, strength
+            statsList = [0,0,0,0,0,0,0]
+            statTotal = 0
+            isExoticBool = False
+            statDict = socketStatDict[item]["stats"]
+            print(socketStatDict[item]["stats"])
+            print(item)
+            for key, val in statDict.items():
+                statTotal += val.get("value")
+                statTypeStr = self.decodeStatHash(int(key))
+                
+                if statTypeStr == "Intellect":
+                    statsList[5] = int(val.get("value"))
+                if statTypeStr == "Resilience":
+                    statsList[2] = int(val.get("value"))
+                if statTypeStr == "Discipline":
+                    statsList[4] = int(val.get("value"))
+                if statTypeStr == "Recovery":
+                    statsList[3] = int(val.get("value"))
+                if statTypeStr == "Mobility":
+                    statsList[1] = int(val.get("value"))
+                if statTypeStr == "Strength":
+                    statsList[6] = int(val.get("value"))
+            statsList[0] = statTotal
+            print(statsList)
+
+            exArmorPerk = None
+
+            print("[ID]   ",item)
+            print("[HASH] ",IDtoArmorHashDict[item])
+            stats = apiResponse304Json["Response"]["itemComponents"]["stats"]["data"].get(item)
+            armorData = await self.decodeHash(IDtoArmorHashDict[item],"DestinyInventoryItemDefinition")
+            print(stats)
+            print(armorData)
+            
+            loreHash = armorData.get("loreHash")
+            if(loreHash == None):
+                armorLore = None
+            else:
+                armorLore = await self.decodeHash(str(loreHash),"DestinyLoreDefinition")
+                armorLore = armorLore["displayProperties"].get("description")
+
+            try:
+                print("[HASH] ",IDtoArmorHashDict[item])
+                
+                armorType = armorData["itemTypeDisplayName"]
+                
+                
+
+                jsonTemplate = {
+                    "name":armorData["displayProperties"].get("name"),
+                    "type":armorData.get("itemTypeAndTierDisplayName"),
+                    "description":armorData["displayProperties"].get("description"),
+                    "lore":armorData.get("flavorText"),
+                    "ExtLore":armorLore,
+                    "itemHash":str(IDtoArmorHashDict[item]),
+                    "itemRating":None,
+                    "icon":"https://www.bungie.net"+str(armorData["displayProperties"].get("icon")),
+                    "backgroundImage" : "https://www.bungie.net"+str(armorData.get("screenshot")),
+                    "rarity":armorData["inventory"].get("tierTypeName"),
+                    "class":armorData.get("classType"), 
+                    "statRolls":statsList,
+                    "weaponPerks":[],
+                    "exoticArmorPerk":exArmorPerk,
+                    "legendWeaponFrame":None,
+                    "legendWeaponDamageType":None,
+                    "legendWeaponDamageElement":None,
+                    "masterworkData":None,
+                }
+            except KeyError:
+                pass
+
+            #store values
+            if(classType == "warlock"):
                 if(armorType == "Helmet" and jsonTemplate["rarity"] != "Exotic"):
                     self.WarlockHelmet = jsonTemplate
                 if(armorType == "Gauntlets" and jsonTemplate["rarity"] != "Exotic"):
@@ -778,8 +935,6 @@ class main:
                 if(armorType == "Warlock Bond" and jsonTemplate["rarity"] != "Exotic"):
                     self.WarlockClassItem = jsonTemplate
             if(classType == "hunter"):
-                if(isExoticBool):
-                    self.HunterExotic = jsonTemplate
                 if(armorType == "Helmet" and jsonTemplate["rarity"] != "Exotic"):
                     self.HunterHelmet = jsonTemplate
                 if(armorType == "Gauntlets" and jsonTemplate["rarity"] != "Exotic"):
@@ -791,8 +946,6 @@ class main:
                 if(armorType == "Hunter Cloak" and jsonTemplate["rarity"] != "Exotic"):
                     self.HunterClassItem = jsonTemplate
             if(classType == "titan"):
-                if(isExoticBool):
-                    self.TitanExotic = jsonTemplate
                 if(armorType == "Helmet" and jsonTemplate["rarity"] != "Exotic"):
                     self.TitanHelmet = jsonTemplate
                 if(armorType == "Gauntlets" and jsonTemplate["rarity"] != "Exotic"):
@@ -837,7 +990,11 @@ class main:
                 exoticCatalysts.append(jsonTemplate)
         self.FirstCatalyst = exoticCatalysts[0]
         self.SecondCatalyst = exoticCatalysts[1]
-        
+
+    async def getXurStrangeGearOffers(self):
+        return None
+   
+
 
     #determines if an item already exists in the json before its added
     def jsonConflictCheck(self,jsonToTest):
@@ -906,9 +1063,14 @@ class main:
         #input("\n")
         #store the inventory of for sale items to be parsed 
         self.forSaleItems = self.apiResponseJson["Response"]["sales"]["data"]
-        await self.getArmor(self.warlockCharacterID,"warlock")
-        await self.getArmor(self.hunterCharacterID,"hunter")
-        await self.getArmor(self.titanCharacterID,"titan")
+        await self.getExoticArmor(self.warlockCharacterID,"warlock")
+        await self.getExoticArmor(self.hunterCharacterID,"hunter")
+        await self.getExoticArmor(self.titanCharacterID,"titan")
+    
+        await self.getLegendaryArmor(self.warlockCharacterID,"warlock")
+        await self.getLegendaryArmor(self.hunterCharacterID,"hunter")
+        await self.getLegendaryArmor(self.titanCharacterID,"titan")
+        
         await self.getWeapons()
         await self.getExoticCatalysts()
 
